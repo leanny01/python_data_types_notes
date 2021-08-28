50169055d295477a51e74210f6cce683ee6b866f
Dibwe <dleannykalangu@gmail.com>
2021-08-28 10:33:58 +0000

    worked for loops

--- loops.py
+++ loops.py
@@ -2,8 +2,24 @@ list = [1,3,4,5,7,89]
 
 # Foor loop
 
-for number in list:
-    if(number % 2 == 0):
-        pass
-        print(number)
-    else   
+# for number in list:
+#     if(number % 2 == 0):
+#         continue
+#     print(number)
+     
+# for number in list:
+#     if(number % 2 == 0):
+#         continue
+#     print(number)
+
+n = 0
+while(n < len(list)-1):
+  n = n + 1
+  #print(list[n])
+  if(list[n] % 2 != 0):
+    continue
+   
+  print(list[n])
+  
+# print the sum
+# print("The sum is", sum) 
---
+++ range.py
@@ -0,0 +1,12 @@
+# list= [1,2,3,5,6,7,8,104,111,1001]
+genre = ['pop', 'rock', 'jazz']
+
+for i in range(len(genre)):
+  print(genre[i])
+
+# for i in genre:
+#   print(i)
+
+# print(list(range(10)))
+# print(list(range(2,8)))
+# print(list(range(len(genre))))
